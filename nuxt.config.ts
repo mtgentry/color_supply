5c3a5319f1f4ef78eca6c37dd1fead955e0e6364
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-03-13 21:32:04 +0200

    add auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      BASE_URL: process.env.BASE_URL,
+      baseURL: process.env.BASE_URL,
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: [
     '@pinia/nuxt',
+    '@sidebase/nuxt-auth',
     '@nuxt/image',
     'vuetify-nuxt-module',
     '@nuxtjs/google-fonts',
@@ -15,4 +29,25 @@ export default defineNuxtConfig({
       'IBM Plex Sans': true,
     }
   },
+  auth: {
+    globalAppMiddleware: {
+      isEnabled: false,
+    },
+    session: {
+      enableRefreshOnWindowFocus: false,
+    },
+    baseURL: process.env.BASE_URL,
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login/', method: 'post' },
+        signOut: false,
+        signUp: false,
+        getSession: { path: '/session/', method: 'get' },
+      },
+      token: {
+        maxAgeInSeconds: 60 * 60 * 24 * 7,
+      }
+    },
+  }
 })
