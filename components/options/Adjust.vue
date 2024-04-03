edcf17726cf8fa0b5c64a62e2fcf197260418903
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-04-04 02:19:15 +0300

    remove swatches

--- components/options/Adjust.vue
+++ components/options/Adjust.vue
@@ -2,7 +2,7 @@
   h2 Adjust
   h3(v-if="store.selectedColor !== null") Hue
   div(v-if="store.selectedColor !== null")
-    v-color-picker(v-model="color" mode="hsla" @update:modelValue="change"  width="100%" show-swatches elevation="0" )
+    v-color-picker(v-model="color" mode="hsla" @update:modelValue="change"  width="100%"  elevation="0" )
     //v-slider(v-model="color.s" label="Saturation" min="0.01" max="1" step="0.01" )
     //v-slider(v-model="color.l" label="Lightness" min="0.01" max="0.99" step="0.01")
 </template>
