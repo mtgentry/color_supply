2ece507c0233508830115c04726adf8ee25919ea
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-07-02 02:19:07 +0300

    add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,29 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      script: [
+        { src: 'https://cdn.amplitude.com/libs/analytics-browser-2.7.4-min.js.gz' },
+        { src: 'https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.4.1-min.js.gz' },
+        { src: 'https://cdn.amplitude.com/libs/plugin-autocapture-browser-0.9.0-min.js.gz'},
+        {
+          hid: 'amplitude',
+          innerHTML: `window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1})).promise.then(function() {window.amplitude.add(window.amplitudeAutocapturePlugin.plugin());window.amplitude.init('d5a19edbf9faf9ec5d29d4b3ec8c5441');});`,
+          type: 'text/javascript',
+          charset: 'utf-8'
+        },
+        {
+          hid: 'heap',
+          innerHTML: `
+            window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+            heap.load("122057296");
+          `,
+          type: 'text/javascript',
+          charset: 'utf-8'
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        amplitude: ['innerHTML']
+      }
     },
     pageTransition: {
       name: 'fade',
@@ -27,7 +50,7 @@ export default defineNuxtConfig({
     layoutTransition: {
       name: 'slide',
       mode: 'out-in' // default
-    }
+    },
   },
   modules: [
     '@pinia/nuxt',
