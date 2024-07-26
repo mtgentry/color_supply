b4658d9bc8c0ff2eb30bea0cab47324c0264988d
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-07-26 09:47:08 +0300

    change plan layout sizes

--- pages/manage/plan.vue
+++ pages/manage/plan.vue
@@ -4,12 +4,13 @@
       v-row(v-if="status")
         v-col(cols="12")
           v-alert(:text="alertMessage" type='info' closable v-if="status === 'success'" icon="mdi-check-circle" )
-      v-row.plans
+      v-row
         v-col#toggle.centered.flex-row(cols="12")
           v-btn(v-for="interval in intervals" :key="interval" flat :variant="selectedInterval === interval ? undefined : 'outlined'"
             :color="selectedInterval === interval ? 'primary': 'gray'"
           @click="selectInterval(interval)") {{capitalized(interval)}}ly
-        v-col.centered(md="3" sm="6" sx="12" cols="12" v-for="plan in plans" :key="plan.name")
+      v-row.plans
+        v-col.centered(lg="3" md="6" sm="6" sx="12" cols="12" v-for="plan in plans" :key="plan.name")
           ManagePlan(:plan="plan")
 
 </template>
@@ -41,6 +42,8 @@ const alertMessage = computed(() => {
     margin-right: 12px
     width: 120px
 .plans
+  :deep(.v-card)
+    width: 256px
   @media (min-width: 1904px)
-    padding: 0 220px
+    padding: 0 96px
 </style>
