0aa56fcb7287ec8b0de08723e46e142b246d3db1
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-09-20 13:52:59 +0200

    #101 - color change

--- components/explore/PreviewInfo.vue
+++ components/explore/PreviewInfo.vue
@@ -4,7 +4,7 @@
       v-col.pa-0.centered.clickable(cols="3" v-for="color in palette?.colors" @click="clipboard(color, snackbar)")
         svg(width="60" height="60" viewBox="0 0 60 60")
           circle(r="28" cx="30" cy="30" :fill="color" :stroke="color === '#FFFFFF'? 'var(--color3)' : ''" stroke-width="1")
-        div.pt-1 {{ color.toUpperCase() }}
+        div.pt-1.hex {{ color.toUpperCase() }}
 </template>
 
 <script setup>
@@ -16,4 +16,6 @@ const { palette, info } = storeToRefs(colorStore)
 <style scoped lang="sass">
 .v-row
   padding: 15px 10px 0 10px
+.hex
+  color: var(--color3)
 </style>
