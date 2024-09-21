5bfdb5c348f2031e5d32d446214a6daaf32d6262
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-09-21 13:06:09 +0200

    remove # from colors

--- components/explore/PreviewInfo.vue
+++ components/explore/PreviewInfo.vue
@@ -4,7 +4,7 @@
       v-col.pa-0.centered.clickable(cols="3" v-for="color in palette?.colors" @click="clipboard(color, snackbar)")
         svg(width="60" height="60" viewBox="0 0 60 60")
           circle(r="28" cx="30" cy="30" :fill="color" :stroke="color === '#FFFFFF'? 'var(--color3)' : ''" stroke-width="1")
-        div.pt-1.hex {{ color.toUpperCase() }}
+        div.pt-1.hex {{ color.toUpperCase().replace('#', '') }}
 </template>
 
 <script setup>
