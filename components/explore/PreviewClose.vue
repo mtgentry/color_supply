a6f182ee1d07f2293cd9c8d87f2a315d37490ac8
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-09-24 13:16:32 +0200

    change close button

--- components/explore/PreviewClose.vue
+++ components/explore/PreviewClose.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
-  svg#close-icon(width='34', height='34', viewBox='0 0 34 34', fill='none', xmlns='http://www.w3.org/2000/svg' @click="closePreview")
-    circle(cx='17', cy='17', r='17', fill='transparent')
-    path(d='M12.001 12.2778L21.7892 22.0661', :stroke='closeColor')
-    path(d='M21.7893 12.2778L12.0011 22.0661', :stroke='closeColor')
+  v-btn.pa-0#close-icon(flat)
+    svg(width='34', height='34', viewBox='0 0 34 34', fill='none',
+      xmlns='http://www.w3.org/2000/svg' @click="closePreview")
+      circle(cx='17', cy='17', r='17', fill='transparent')
+      path(d='M12.001 12.2778L21.7892 22.0661', :stroke='closeColor')
+      path(d='M21.7893 12.2778L12.0011 22.0661', :stroke='closeColor')
 </template>
 
 <script setup>
@@ -31,8 +33,17 @@ const closeColor = computed(() => {
 <style scoped lang="sass">
 #close-icon
   position: absolute
-  top: 0
-  right: 0
+  top: 2px
+  right: 2px
   cursor: pointer
   z-index: 100
+  padding: 0!important
+  height: 34px
+  width: 34px
+  min-width: 34px
+  background-color: transparent
+  border-radius: 100%
+  opacity: 0
+  &:hover
+    opacity: 1
 </style>
