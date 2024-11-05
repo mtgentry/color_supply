4c2c1495553da3c36ac30e2e4bfd2bf492cbccb7
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-11-05 14:10:20 +0100

    change color filter padding

--- components/explore/harmony/Color.vue
+++ components/explore/harmony/Color.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .centered.clickable(@click="select")
     svg(width="30" height="30" viewBox="0 0 30 30")
-      circle(r="14" cx="15" cy="15" :fill="color" :stroke="title === 'white' ? 'var(--color3)' : ''" stroke-width="1")
+      circle(r="15" cx="15" cy="15" :fill="color" :stroke="title === 'white' ? 'var(--color3)' : ''" stroke-width="1")
       path(d="M16.0605 1.06055L5.7959 11.3252L1 6.5293" transform="translate(7,9)" :stroke="title === 'white' ? 'var(--color3)': 'white'"
         stroke-width="2" fill="transparent" v-if="colors.includes(color)")
     div.color-name {{ capitalized(title) }}
@@ -23,5 +23,7 @@ const select = () => {
 
 <style scoped lang="sass">
 .color-name
-  font-size: 14px
+  margin-top: 8px
+  font-size: 0.813rem
+  line-height: 0.4
 </style>
