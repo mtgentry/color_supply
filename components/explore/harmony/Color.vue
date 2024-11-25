5d7011b1924a52d045667f146add623467b5cc3f
Viacheslav Khromiak <slava.khromyak@gmail.com>
2024-11-25 13:16:39 +0100

    146 - circle issue

--- components/explore/harmony/Color.vue
+++ components/explore/harmony/Color.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .centered.clickable(@click="select")
     svg(width="30" height="30" viewBox="0 0 30 30")
-      circle(r="15" cx="15" cy="15" :fill="color" :stroke="title === 'white' ? 'var(--color3)' : ''" stroke-width="1")
+      circle(r="14" cx="15" cy="15" :fill="color" :stroke="title === 'white' ? 'var(--color3)' : ''" stroke-width="1")
       path(d="M16.0605 1.06055L5.7959 11.3252L1 6.5293" transform="translate(7,9)" :stroke="title === 'white' ? 'var(--color3)': 'white'"
         stroke-width="2" fill="transparent" v-if="colors.includes(color)")
     div.color-name {{ capitalized(title) }}
